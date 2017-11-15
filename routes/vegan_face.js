7599477b8358500261b22ade1bfd187af6bd8808
ronyehezkel90 <ronyehezkel90@gmail.com>
2017-11-15 18:32:10 +0200

    change path for machine

--- routes/vegan_face.js
+++ routes/vegan_face.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 var path;
-var local = true;
+var local = false;
 if (local) {
     path = "/home/ron/PycharmProjects/vegan_face/router.py";
 }
 else {
     //todo: Change path to git path
-    path = "/home/ec2-user/vegan_face_python/router.py";
+    path = "/home/ubuntu/vegan_app/vegan_face_python/router.py";
 }
 router.get('/', function (req, res, next) {
     console.log("top_rests requests has been sent");
